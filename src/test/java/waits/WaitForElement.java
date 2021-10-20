479744dbe56083bc34c8772785a792176aa1665b
Jarekzielinski <jaroslawzielinski1986@wp.pl>
2021-10-20 12:25:36 +0200

    Added Wait class

---
+++ src/test/java/waits/WaitForElement.java
@@ -0,0 +1,21 @@
+package waits;
+
+import driver.manager.DriverManager;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public class WaitForElement {
+
+    private static WebDriverWait getWebDriverWait(){
+        return new WebDriverWait(DriverManager.getWebDriver(), 10);
+    }
+    public static void waitUntilElementIsVisible(WebElement webElement){
+        getWebDriverWait().until(ExpectedConditions.visibilityOf(webElement));
+    }
+    public static void  waitForElementIsClickable(WebElement webElement){
+        getWebDriverWait().until(ExpectedConditions.elementToBeClickable(webElement));
+    }
+}
