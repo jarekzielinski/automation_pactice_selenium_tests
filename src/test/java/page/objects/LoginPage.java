8741df99b295e8dfdb50474907cd91e6b878dce4
Jarekzielinski <jaroslawzielinski1986@wp.pl>
2021-10-20 13:35:48 +0200

    Added removed Login class

---
+++ src/test/java/page/objects/LoginPage.java
@@ -0,0 +1,42 @@
+package page.objects;
+
+import driver.manager.DriverManager;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import waits.WaitForElement;
+
+public class LoginPage {
+    @FindBy(id = "email")
+    private WebElement email;
+    @FindBy(id = "passwd")
+    private WebElement pass;
+    @FindBy(id = "SubmitLogin")
+    private WebElement submitButton;
+    @FindBy(css = "div[class='alert alert-danger'] p")
+    private WebElement errorField;
+
+    public LoginPage() {
+        PageFactory.initElements(DriverManager.getWebDriver(), this);
+    }
+
+    public void typeIntoUsernameField(String name) {
+        WaitForElement.waitUntilElementIsVisible(email);
+        email.sendKeys(name);
+    }
+
+    public void typeIntoPasswordField(String password) {
+        WaitForElement.waitUntilElementIsVisible(pass);
+        pass.sendKeys(password);
+    }
+
+    public void clickOnLoginButton() {
+        WaitForElement.waitForElementIsClickable(submitButton);
+        submitButton.click();
+    }
+
+    public String getMessage() {
+        WaitForElement.waitUntilElementIsVisible(errorField);
+        return errorField.getText();
+    }
+}
